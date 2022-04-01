8536c90d387e587eb59030cc329a5146fb7b37f7
rufiz_mardan <rufiz.mardan@gmail.com>
2022-04-01 18:12:40 +0200

    Create test_get_post_valid

--- tests/posts_test.py
+++ tests/posts_test.py
@@ -3,7 +3,7 @@ import pytest
 
 from fastapi import status
 
-from app.schemas import PostDB
+from app.schemas import PostDB, PostPartialUpdate
 
 
 @pytest.mark.asyncio
@@ -14,30 +14,28 @@ class TestPosts:
         # Assert it returns 200 status code
         assert response.status_code == status.HTTP_200_OK
 
-    async def test_get_post_valid(self):
-        # Create a post
-        # Return the created post
-        # Delete the created post
-        pass
+    async def test_get_post_valid(self, test_client: httpx.AsyncClient, test_create_post):
+        # Create a post and read the response
+        response = test_create_post
+        response_json = response.json()
+        # Get the created post
+        extracted_post = await test_client.get(f'/posts/{response_json.get("id")}')
+        extracted_post_json = extracted_post.json()
+        # Assert if the status code and the content match
+        assert extracted_post.status_code == status.HTTP_200_OK
+        assert extracted_post_json.get('title') == 'Pytest Fixture'
+        assert extracted_post_json.get('title') == 'Created with Pytest'
 
-    async def test_create_post_valid(self):
+    async def test_update_post_valid(self, test_client: httpx.AsyncClient, test_create_post):
         # Create a post
-        # Read the response content
-        # Assert the status code 201 and content of
-        # the response post is the same as the created one
-        pass
-
-    async def test_update_post_valid(self):
-        # Create a post
-        # Update it
+        response = test_create_post
+        response_json = response.json()
+        # Update the created post
+        post_to_update = PostPartialUpdate(title='Update with Pytest',
+                                           content='Pytest is awesome').dict()
+        # Todo: finalize the tests for TestPosts
         # Assert the response from the updated post is the same as
         # the response post
-        pass
-
-    async def test_delete_post_valid(self):
-        # Create a post
-        # Delete the post
-        # Assert the response code is 204
         pass
 
 
